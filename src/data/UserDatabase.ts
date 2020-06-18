84c3354957f374b2cdf45a0c31733601bee9ef49
igordelesposti <igordelesposti@gmail.com>
2020-06-18 16:44:07 -0300

    Segunda entrega do projeto

--- src/data/UserDatabase.ts
+++ src/data/UserDatabase.ts
@@ -1,9 +1,11 @@
 import { BaseDatabase } from "./BaseDataBase";
+import { stringify } from "querystring";
 
 export class UserDatabase extends BaseDatabase {
   
 
   private static TABLE_NAME = "cookenu_user";
+  private static TABLE_NAME_FOLLOW = "cookenu_follow";
 
   public async createUser(
     id: string,
@@ -37,5 +39,16 @@ export class UserDatabase extends BaseDatabase {
       .where({ id });
 
     return result[0];
+  }
+
+  public async followUserById(id_follower: string, id_followed: string): Promise<any>{
+    const result = await this.getConnection()
+      .insert({
+        id_followed,
+        id_follower
+      })
+      .into(UserDatabase.TABLE_NAME_FOLLOW)
+
+      return(result)
   }
 }
